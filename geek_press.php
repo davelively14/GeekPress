406656a0a27be5938e2bb6ee0e1457efd6b655b4
Dave Lively <davelively14@gmail.com>
2016-04-09 19:45:39 -0400

    Added a few defaults

--- geek_press.php
+++ geek_press.php
@@ -4,8 +4,28 @@
  * Plugin Name: GeekPress
  * Plugin URI: https://github.com/davelively14/GeekPress
  * Description: This is a template tag plugin for WordPress that will connect with BandsInTown (BIT) API.
- * Version: 0.3
+ * Version: 0.1
  * Author: Dave Lively
  */
 
- ?>
+// CONTENT
+// Change these constants to adjust the content to be displayed in the returned
+// HTML code for given functions.
+//
+
+// DEFAULTS
+// These constants set defaults for many of the functions.
+//
+// Sets the default city for geographic searches
+define('DEFAULT_CITY_STR', 'Atlanta');
+// Sets the default state for geographic searches
+define('DEFAULT_STATE_STR', 'GA')
+// Sets the default URL parameter to identify affiliation
+define('DEFAULT_URL_PARAM', '?aid=11390')
+
+// SYSTEM CONSTANTS
+// Don't change these. These are SeatGeek API specific
+
+
+
+?>
