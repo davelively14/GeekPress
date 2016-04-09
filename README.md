57a3090b8326f6c8a6c0d9b3abf9a2a3db61e4a3
Dave Lively <davelively14@gmail.com>
2016-04-09 19:35:44 -0400

    Initial commits

---
+++ README.md
@@ -0,0 +1,4 @@
+# Geek Press
+**A way to use php within your WordPress app to pull data from SeatGeek's API**
+
+Current Version: 0.1
---
+++ geek_press.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Plugin Name: GeekPress
+ * Plugin URI: https://github.com/davelively14/GeekPress
+ * Description: This is a template tag plugin for WordPress that will connect with BandsInTown (BIT) API.
+ * Version: 0.3
+ * Author: Dave Lively
+ */
+
+ ?>
